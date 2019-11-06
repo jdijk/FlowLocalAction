123e8765edeb776139e670075a50bc7418d454b5
Jack van Dijk <jack@jackvandijk.nl>
2019-11-06 12:05:52 +0100

    Update showToastController.js

--- src/aura/showToast/showToastController.js
+++ src/aura/showToast/showToastController.js
@@ -4,10 +4,13 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root  or https://opensource.org/licenses/BSD-3-Clause
  */
- ({
+({
     invoke : function(component, event, helper) {
+        var args = event.getParam("arguments");
+
         var message = component.get("v.message");
         var type = component.get("v.type");
         helper.showToast(type, message);
-      }
+
+    }
 })
